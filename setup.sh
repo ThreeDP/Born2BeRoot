e05f9090fe922bde99dd1d3a79708bd6440c0c01
threedp <davy.paulino@aluno.ifsp.edu.br>
2023-01-04 19:22:17 -0300

    last att

--- setup.sh
+++ setup.sh
@@ -10,12 +10,13 @@ usermod -aG sudo dapaulin
 # check if the user is in sudo group
 getent group sudo
 # privilege for a sudo user
-sudo visudo
-
+sudo apt-get install -y git wget
+sh -c "$(wget https://raw.githubusercontent.com/ThreeDP/Born2BeRoot/main/sudo_config.sh -O -)"
+./sudo_config.sh
 
 # Installing tools
 sudo apt-get update -y && apt-get upgrade -y 
-sudo apt-get install -y git wget vim openssh-server
+sudo apt-get install -y vim openssh-server
 sed -i 's/#Port 22/Port 4242/g' /etc/ssh/sshd_config
 service ssh restart
 sudo apt-get install ufw -y
