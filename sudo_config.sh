3d30a4c03d8b71d2a25d8ba1b916733aadb37331
threedp <davy.paulino@aluno.ifsp.edu.br>
2023-01-05 16:11:32 -0300

    test

--- sudo_config.sh
+++ sudo_config.sh
@@ -3,9 +3,11 @@
 echo -e '#My setup\ndapaulin  ALL=(ALL:ALL) ALL ' | sudo EDITOR='tee -a' visudo &&
 echo -e '\nDefaults passwd_tries=3' | sudo EDITOR='tee -a' visudo &&
 echo -e 'Defaults badpass_message="Wrong password. Try again!"' | sudo EDITOR='tee -a' visudo
-if [ ! -d/var/log/sudo ]; then
-    echo "create dir"
+if [ -d /var/log/sudo ]; then
+    echo "directory exists"
+else
     mkdir /var/log/sudo
+    echo "directory created"
 fi
 echo -e 'Defaults logfile="/var/log/sudo/sudo.log"' | sudo EDITOR='tee -a' visudo &&
 echo -e 'Defaults log_input' | sudo EDITOR='tee -a' visudo &&
