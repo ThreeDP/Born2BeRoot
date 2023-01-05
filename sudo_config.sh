17b5fc5d650af301a7171296fb8b3228b94a4433
threedp <davy.paulino@aluno.ifsp.edu.br>
2023-01-05 15:24:47 -0300

    test

--- sudo_config.sh
+++ sudo_config.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # necessário está no modo root.
-echo -e '#My setup\ndapaulin  ALL=(ALL:ALL) ALL ' | sudo EDITOR='tee -a' visudo &&
-echo -e '\nDefaults passwd_tries=3' | sudo EDITOR='tee -a' visudo &&
-echo -e 'Defaults badpass_message="Wrong password. Try again!"' | sudo EDITOR='tee -a' visudo
+echo -e "#My setup\ndapaulin  ALL=(ALL:ALL) ALL " | sudo EDITOR="tee -a" visudo &&
+echo -e "\nDefaults passwd_tries=3" | sudo EDITOR="tee -a" visudo &&
+echo -e "Defaults badpass_message='Wrong password. Try again!'" | sudo EDITOR="tee -a" visudo
 if [ ! -d/var/log/sudo ]; then
     mkdir /var/log/sudo
 fi
-echo -e 'Defaults logfile="/var/log/sudo/sudo.log"' | sudo EDITOR='tee -a' visudo &&
-echo -e 'Defaults log_input' | sudo EDITOR='tee -a' visudo &&
-echo -e 'Defaults log_output' | sudo EDITOR='tee -a' visudo &&
-echo -e 'Defaults requiretty' | sudo EDITOR='tee -a' visudo
+echo -e "Defaults logfile='/var/log/sudo/sudo.log'" | sudo EDITOR="tee -a" visudo &&
+echo -e "Defaults log_input" | sudo EDITOR="tee -a" visudo &&
+echo -e "Defaults log_output" | sudo EDITOR="tee -a" visudo &&
+echo -e "Defaults requiretty" | sudo EDITOR="tee -a" visudo
