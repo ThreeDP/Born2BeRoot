d64bc618716816d2c1bf8c5376ef6a551299be8d
threedp <davy.paulino@aluno.ifsp.edu.br>
2023-01-06 07:45:14 -0300

    alter

--- ssh_config.sh
+++ ssh_config.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 sed -i 's/#Port 22/Port 4242/g' /etc/ssh/sshd_config &&
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin no/g' /etc/ssh/sshd_config &&
 sudo systemctl restart ssh
 sudo rm ssh_config.sh
+sudo apt-get install net-tools -y &&
