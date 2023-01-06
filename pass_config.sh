23ec86b56e7d7a9c850a5a0137d32aacc9b8ab74
threedp <davy.paulino@aluno.ifsp.edu.br>
2023-01-06 14:28:08 -0300

    Configure the password policy script

--- pass_config.sh
+++ pass_config.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 sudo sed -i 's/PASS_MAX_DAYS/#PASS_MAX_DAYS/g' /etc/login.defs &&
 sudo sed -i 's/PASS_MIN_DAYS/#PASS_MIN_DAYS/g' /etc/login.defs &&
-sudo sed -i 's/PASS_WARN_AGE\t7/PASS_MAX_DAYS\t30\nPASS_MIN_DAYS\t2\nPASS_WARN_AGE\t7/g' /etc/login.defs
+sudo sed -i 's/PASS_WARN_AGE\t7/PASS_MAX_DAYS\t30\nPASS_MIN_DAYS\t2\nPASS_WARN_AGE\t7/g' /etc/login.defs &&
+sudo chage -M 30 dapaulin && sudo chage -M 30 root
+sudo chage -m 2 dapaulin && sudo chage -m 2 root
+sudo chage -W 7 dapaulin && sudo chage -W 7 root
+sudo apt-get install libpam-pwquality &&
+sudo sed -i 's/#difok = \([0-9]\{1,\}\)/difok = 7/g' /etc/security/pwquality.conf &&
+sudo sed -i 's/#minlen = \([0-9]\{1,\}\)/minlen = 10/g' /etc/security/pwquality.conf &&
+sudo sed -i 's/#dcredit = \([0-9]\{1,\}\)/dcredit = -1/g' /etc/security/pwquality.conf &&
+sudo sed -i 's/#ucredit = \([0-9]\{1,\}\)/ucredit = -1/g' /etc/security/pwquality.conf &&
+sudo sed -i 's/#maxrepeat = \([0-9]\{1,\}\)/maxrepeat = 3/g' /etc/security/pwquality.conf &&
+sudo sed -i 's/#usercheck = \([0-9]\{1,\}\)/usercheck = 1/g' /etc/security/pwquality.conf &&
+sudo sed -i 's/#retry = \([0-9]\{1,\}\)/retry = 3/g' /etc/security/pwquality.conf &&
+
