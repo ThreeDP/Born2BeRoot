da178e3d67d46cc8d2d447461b3aac5eec963717
threedp <davy.paulino@aluno.ifsp.edu.br>
2023-01-06 10:40:14 -0300

    test

--- pass_config.sh
+++ pass_config.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo sed -i 's/PASS_MAX_DAYS/#PASS_MAX_DAYS/g' &&
-sudo sed -i 's/PASS_MIN_DAYS/#PASS_MIN_DAYS/g' &&
-sudo sed -i 's/PASS_WARN_AGE\t7/PASS_MAX_DAYS\t30\nPASS_MIN_DAYS\t2\nPASS_WARN_AGE\t7/g'
+sudo sed -i 's/PASS_MAX_DAYS/#PASS_MAX_DAYS/g' /etc/login.defs &&
+sudo sed -i 's/PASS_MIN_DAYS/#PASS_MIN_DAYS/g' /etc/login.defs &&
+sudo sed -i 's/PASS_WARN_AGE\t7/PASS_MAX_DAYS\t30\nPASS_MIN_DAYS\t2\nPASS_WARN_AGE\t7/g' /etc/login.defs
